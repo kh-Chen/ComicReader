175c65630163427d12e11d98b559881f9532bf71
chenkuihan <chenkuihan@360.cn>
2023-06-29 15:54:51 +0800

    delete args

--- src/main/java/com/chenkh/comicreader/ComicreaderApplication.java
+++ src/main/java/com/chenkh/comicreader/ComicreaderApplication.java
@@ -2,13 +2,19 @@ package com.chenkh.comicreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 public class ComicreaderApplication {
 
     public static void main(String[] args) {
-        System.setProperty("jsonPath", args[0]);
-        System.setProperty("imgPath", args[1]);
+        String jsonPath = System.getProperty("jsonPath");
+        String imgPath = System.getProperty("imgPath");
+        if (!StringUtils.hasLength(jsonPath) || !StringUtils.hasLength(imgPath)) {
+            System.out.println("jsonPath and imgPath is empty");
+            return;
+        }
+        
         SpringApplication.run(ComicreaderApplication.class, args);
     }
 
