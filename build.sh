5f2b101ff0796e7af95fe70000f1b5328a5a2e36
khchen <18241224605@163.com>
2023-07-03 21:43:50 +0800

    config build-no-fork

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mvn clean -Pnative native:compile-no-fork
+mvn clean package -Pnative
+#mvn clean -Pnative native:compile-no-fork -e -X
 #java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar target/comicreader-1.0.jar /var/www/HanMan/books.json /var/www/HanMan/images
 #./comicreader /var/www/HanMan/books.json /var/www/HanMan/images
